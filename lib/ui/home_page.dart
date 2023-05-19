09006c60fa341e05cb965251c2bad7843351518d
Somrat Hossain <somrat945hossain@gmail.com>
2023-05-20 02:17:13 +0600

    home screen notes card add and delete note done initially

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sticky_note/provider/subject_provider.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final subjectProvider = Provider.of<SubjectProvider>(context);
+    final subjectProvider = Provider.of<SubjectProvider>(context, listen: false);
+    print('build');
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
@@ -16,18 +16,35 @@ class HomePage extends StatelessWidget {
         subjectProvider.addSubjct('biology');
       }),
       appBar: AppBar(
-        title: Text('Sticky Notes'),
+        title:  Consumer<SubjectProvider>(
+          builder: (context, value, child) => 
+           Text('Sticky Notes ${subjectProvider.notes.length + 1}')),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          Expanded(child: ListView.builder(
-            itemCount: subjectProvider.notes.length,
-            itemBuilder: (_, index){
-            return Card(
-              child: Text(subjectProvider.notes[index].subject),
-            );
-          }))
+          Expanded(child: Consumer<SubjectProvider>(
+            builder: (context, value, child) => 
+            ListView.builder(
+              itemCount: subjectProvider.notes.length,
+              itemBuilder: (_, index){
+              return Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(subjectProvider.notes[index].subject, style: TextStyle(
+                      fontSize: 25,
+                    ),),
+                    IconButton(
+                      onPressed: (){
+                          subjectProvider.deleteSubject(index);
+                      },
+                      icon: Icon(Icons.delete, size: 27,)),
+                  ],
+                ),
+              );
+            }),
+          ))
         ],
       ),
       
